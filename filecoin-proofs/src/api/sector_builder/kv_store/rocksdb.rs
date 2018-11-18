47aa5a93435919081079120971b1c209be024d1c
Erin Swenson-Healey <l@s3r.me>
2018-11-18 14:00:38 -0800

    add basic test of rocksdb impl

--- filecoin-proofs/src/api/sector_builder/kv_store/rocksdb.rs
+++ filecoin-proofs/src/api/sector_builder/kv_store/rocksdb.rs
@@ -35,3 +35,27 @@ impl KeyValueStore for RocksDb {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use api::sector_builder::kv_store::KeyValueStore;
+    use api::sector_builder::kv_store::rocksdb::RocksDb;
+
+    #[test]
+    fn test_alpha() {
+        let metadata_dir = tempfile::tempdir().unwrap();
+
+        let db = RocksDb::new(metadata_dir).unwrap();
+
+        let k_a = b"key-xx";
+        let k_b = b"key-yy";
+        let v_a = b"value-aa";
+        let v_b = b"value-bb";
+
+        db.put(k_a, v_a).unwrap();
+        db.put(k_b, v_b).unwrap();
+
+        let opt = db.get(k_a).unwrap();
+        assert_eq!(format!("{:x?}", opt.unwrap()), format!("{:x?}", v_a));
+    }
+}
