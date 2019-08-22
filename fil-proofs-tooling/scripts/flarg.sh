46c3fbb9d6940f789324c5a538e8fdde07fa3d42
dignifiedquire <dignifiedquire@users.noreply.github.com>
2019-08-22 18:33:28 +0200

    enable disk-trees for flarg

--- fil-proofs-tooling/scripts/flarg.sh
+++ fil-proofs-tooling/scripts/flarg.sh
@@ -7,5 +7,5 @@ RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=a
 for t in ${post_sector_counts[@]}; do
   sed -i -E "s/^pub const POST_SECTORS_COUNT.*$/pub const POST_SECTORS_COUNT: usize = $t;/" filecoin-proofs/src/constants.rs
   echo "sealing and generating PoSts with POST_SECTORS_COUNT=$t"
-  RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 8 1
+  RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release --features disk-trees -- 8 1
 done
