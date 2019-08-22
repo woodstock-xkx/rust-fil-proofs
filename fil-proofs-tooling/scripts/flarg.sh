5f8d025724943afc22d545d870f34258ccc0f7b4
laser <l@s3r.me>
2019-08-21 23:55:20 -0700

    refactor

--- fil-proofs-tooling/scripts/flarg.sh
+++ fil-proofs-tooling/scripts/flarg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-post_sector_counts=(2 4 8 16 32 64)
+post_sector_counts=(2 4 8 16)
 
 for t in ${post_sector_counts[@]}; do
   sed -i -E "s/^pub const POST_SECTORS_COUNT.*$/pub const POST_SECTORS_COUNT: usize = $t;/" filecoin-proofs/src/constants.rs
@@ -9,14 +9,10 @@ for t in ${post_sector_counts[@]}; do
   RUST_BACKTRACE=1 RUST_LOG=info cargo run --color=always --package filecoin-proofs --bin paramcache --release
 done
 
-for t in ${post_sector_counts[@]}; do
-  sed -i -E "s/^pub const POST_SECTORS_COUNT.*$/pub const POST_SECTORS_COUNT: usize = $t;/" filecoin-proofs/src/constants.rs
-  echo "sealing 64 with POST_SECTORS_COUNT=$t"
-  RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 2 1
-done
+RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 16 1
 
 for t in ${post_sector_counts[@]}; do
   sed -i -E "s/^pub const POST_SECTORS_COUNT.*$/pub const POST_SECTORS_COUNT: usize = $t;/" filecoin-proofs/src/constants.rs
   echo "generating PoSts for 64 sectors with POST_SECTORS_COUNT=$t"
-  RUST_BACKTRACE=1 RUST_LOG=info RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 2 0
+  RUST_BACKTRACE=1 RUST_LOG=info RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 16 0
 done
