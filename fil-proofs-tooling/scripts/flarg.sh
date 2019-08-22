909474bcff5d28686b43151f93c52328c69b3188
laser <l@s3r.me>
2019-08-22 13:15:29 -0700

    bump back to 512

--- fil-proofs-tooling/scripts/flarg.sh
+++ fil-proofs-tooling/scripts/flarg.sh
@@ -2,7 +2,7 @@
 
 post_sector_counts=(2 4 8)
 
-RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 8 0
+RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 16 0
 
 for t in ${post_sector_counts[@]}; do
   sed -i -E "s/^pub const POST_SECTORS_COUNT.*$/pub const POST_SECTORS_COUNT: usize = $t;/" filecoin-proofs/src/constants.rs
