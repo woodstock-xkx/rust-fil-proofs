ddb8b4cfc6914922b771b3417789322a29d0de0e
laser <l@s3r.me>
2019-08-22 10:06:45 -0700

    decrease to 8 max

--- fil-proofs-tooling/scripts/flarg.sh
+++ fil-proofs-tooling/scripts/flarg.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-post_sector_counts=(2 4 8 16)
+post_sector_counts=(2 4 8)
 
-RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 16 0
+RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 8 0
 
 for t in ${post_sector_counts[@]}; do
   sed -i -E "s/^pub const POST_SECTORS_COUNT.*$/pub const POST_SECTORS_COUNT: usize = $t;/" filecoin-proofs/src/constants.rs
   echo "sealing and generating PoSts with POST_SECTORS_COUNT=$t"
-  RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 16 1
+  RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 8 1
 done
