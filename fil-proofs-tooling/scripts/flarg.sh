924c10ec072370cbd52cde72262516ffec2d4511
laser <l@s3r.me>
2019-08-22 08:30:46 -0700

    WIP

--- fil-proofs-tooling/scripts/flarg.sh
+++ fil-proofs-tooling/scripts/flarg.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-post_sector_counts=(2 4 8 16 32)
+post_sector_counts=(2 4 8 16)
 
 for t in ${post_sector_counts[@]}; do
   sed -i -E "s/^pub const POST_SECTORS_COUNT.*$/pub const POST_SECTORS_COUNT: usize = $t;/" filecoin-proofs/src/constants.rs
   echo "sealing and generating PoSts with POST_SECTORS_COUNT=$t"
-  RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 32 0
+  RUST_BACKTRACE=1 RUST_LOG=info env time -f "max mem: '%MKB'" cargo run --color=always --package fil-proofs-tooling --bin blarg --release -- 16 0
 done
